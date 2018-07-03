8ade33d180337bc710c53c2af1ce549729b50fd4
nicola baldi (@naighes) <nic.baldi@gmail.com>
2018-07-03 06:40:45 +0200

    testing non-matching rules.

--- RegexHandler_test.go
+++ RegexHandler_test.go
@@ -39,3 +39,22 @@ func TestMatchingRule(t *testing.T) {
 		t.Errorf("expected status code %d; got %d", expected, r.Code)
 	}
 }
+
+func TestNonMatchingRule(t *testing.T) {
+	const expected = 404
+	rsp := MatchRsp{Body: "${text(hello)}", StatusCode: 200}
+	def := MatchDef{Pattern: "^/[0-9]+$", Response: &rsp}
+	defs := []*MatchDef{&def}
+	config := Config{Defs: defs}
+	r := httptest.NewRecorder()
+	routes, err := NewRegexHandler(&config)
+	if err != nil {
+		t.Errorf("cannot create a new instance of NewRegexHandler")
+	}
+	url, _ := url.Parse("http://fak.eurl/aaa")
+	req := http.Request{Method: "GET", URL: url}
+	routes.ServeHTTP(r, &req)
+	if r.Code != expected {
+		t.Errorf("expected status code %d; got %d", expected, r.Code)
+	}
+}
