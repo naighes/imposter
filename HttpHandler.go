bccac1eeb0eff67d68c0789cd4ed795be6555579
nicola baldi (@naighes) <nic.baldi@gmail.com>
2018-07-16 09:57:56 +0200

    evaluating headers.

--- HttpHandler.go
+++ HttpHandler.go
@@ -42,28 +42,51 @@ type MatchRspHttpHandler struct {
 	Content *MatchRsp
 }
 
+func evaluateToString(e expression) (string, error) {
+	a, err := e.evaluate()
+	if err != nil {
+		return "", err
+	}
+	b, ok := a.(string)
+	if !ok {
+		return "", fmt.Errorf("expected a value of type string; got '%s' instead", reflect.TypeOf(a))
+	}
+	return b, nil
+}
+
 func (h MatchRspHttpHandler) HandleFunc(parse func(string) (expression, error)) (func(http.ResponseWriter, *http.Request), error) {
 	rsp := h.Content
 	e, err := parse(rsp.Body)
 	if err != nil {
 		return nil, err
 	}
+	headers := make(map[string]expression)
+	if rsp.Headers != nil {
+		for k, v := range rsp.Headers {
+			header, ok := v.(string)
+			if !ok {
+				return nil, fmt.Errorf("expected a value of type string; got '%s' instead", reflect.TypeOf(v))
+			}
+			he, err := parse(header)
+			if err != nil {
+				return nil, err
+			}
+			headers[k] = he
+		}
+	}
 	return func(w http.ResponseWriter, r *http.Request) {
-		a, err := e.evaluate()
+		b, err := evaluateToString(e)
 		if err != nil {
 			writeError(w, err)
 			return
 		}
-		b, ok := a.(string)
-		if !ok {
-			writeError(w, fmt.Errorf("expected a body of type string; got '%s' instead", reflect.TypeOf(a)))
-			return
-		}
-		if rsp.Headers != nil {
-			// TODO: evaluate headers
-			for k, v := range rsp.Headers {
-				w.Header().Set(k, v.(string))
+		for k, v := range headers {
+			v1, err := evaluateToString(v)
+			if err != nil {
+				writeError(w, err)
+				return
 			}
+			w.Header().Set(k, v1)
 		}
 		w.WriteHeader(rsp.StatusCode)
 		fmt.Fprintf(w, b)
