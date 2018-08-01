fe3d0f4a2994e55ed4075c7b6caee17976448f3d
nicola baldi (@naighes) <nic.baldi@gmail.com>
2018-08-01 07:25:06 +0200

    testing validation stuff.

---
+++ configuration_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNonBooleanRuleExpression(t *testing.T) {
+	rsp := MatchRsp{}
+	def := &MatchDef{RuleExpression: `${"some string"}`, Response: &rsp}
+	vars := make(map[string]interface{})
+	errors := def.validate(vars)
+	const expected = 1
+	if l := len(errors); l != expected {
+		t.Errorf("expected %d error(s); got %d instead", expected, l)
+		return
+	}
+}
+
+func TestBooleanRuleExpression(t *testing.T) {
+	rsp := MatchRsp{}
+	def := &MatchDef{RuleExpression: `${true}`, Response: &rsp}
+	vars := make(map[string]interface{})
+	errors := def.validate(vars)
+	const expected = 0
+	if l := len(errors); l != expected {
+		t.Errorf("expected %d error(s); got %d instead", expected, l)
+		return
+	}
+}
+
+func TestBodyExpressionSyntaxError(t *testing.T) {
+	rsp := MatchRsp{Body: `${var("www"}`}
+	def := &MatchDef{RuleExpression: `${true}`, Response: &rsp}
+	vars := make(map[string]interface{})
+	errors := def.validate(vars)
+	const expected = 1
+	if l := len(errors); l != expected {
+		t.Errorf("expected %d error(s); got %d instead", expected, l)
+		return
+	}
+}
+
+func TestHeaderExpressionSyntaxError(t *testing.T) {
+	h := map[string]interface{}{
+		"Content-Type": "${www}",
+	}
+	rsp := MatchRsp{Headers: h}
+	def := &MatchDef{RuleExpression: `${true}`, Response: &rsp}
+	vars := make(map[string]interface{})
+	errors := def.validate(vars)
+	const expected = 1
+	if l := len(errors); l != expected {
+		t.Errorf("expected %d error(s); got %d instead", expected, l)
+		return
+	}
+}
